58890eac96912e589a823de141c7b9734f2c6c63
Anna Cho <loveablessing@gmail.com>
2022-05-18 17:32:22 -0400

    commit tree a39a8f1f31dff946285c1c89af1b686addb8e55e

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let charge
+  if (someNumber <= 400) {
+    charge = 'This one is on me!';
+  }
+  else if (someNumber >= 400 && someNumber <= 2000) {
+    charge = 'That will be twenty bucks.';
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    charge = 'I will gladly take your thirty bucks.';
+  }
+  else {
+    charge = 'No can do.'
+  }
+  return charge;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  let myAnswer;
+  if (someCity == 'NYC'){
+    myAnswer = 'Ok, sounds good.'
+  }
+  else {
+    myAnswer = 'No go.'
+  }
+  return myAnswer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
+ 
   // Write your code here!
+  let tipper;
+  switch(someTip){
+    case 'generous':
+      tipper = 'Thank you so much.';
+      return tipper;
+      
+    case 'not as generous':
+      tipper = 'Thank you.';
+      return tipper;
+      
+    default:
+      tipper = 'Bye.'
+      return tipper;
+  }
 }
